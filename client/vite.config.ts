baf59d87d500a1f5a8093de818915a05eae1b1d8
formixcode <contact@formixcode.com>
2025-05-11 16:17:33 +0700

    upgrade to TailwindCSS 4

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import autoprefixer from 'autoprefixer'
-import postcss from 'postcss'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
-    
   ],
   css: {
     postcss: {
       plugins: [
         autoprefixer({}),
-        postcss() // add options if needed
+
       ],
     }
   }
