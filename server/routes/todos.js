52fab0a2c174cdc3b7aedbbc15b9b0a808620e04
formixcode <contact@formixcode.com>
2025-05-11 13:59:33 +0700

    add server components

---
+++ server/routes/todos.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const Todo = require("../models/Todo");
+
+router.get("/", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+router.post("/", async (req, res) => {
+  const newTodo = await Todo.create(req.body);
+  res.json(newTodo);
+});
+
+router.put("/:id", async (req, res) => {
+  const updated = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+router.delete("/:id", async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
+
+module.exports = router;
